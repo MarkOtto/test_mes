cf63f97d23e03b8809fe4bddaf99a622f1fc3b36
Mark Otto <markottocom@icloud.com>
2022-01-31 15:14:56 +0300

    Production

---
+++ app/frontend/dist/precache-manifest.4cab62cbf1767dc61b80a14f953b2db4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b721446d0a27cdea94d",
+    "url": "/css/app.627c6701.css"
+  },
+  {
+    "revision": "2d536c46d664fb2c15b2",
+    "url": "/css/chunk-vendors.fe29e39f.css"
+  },
+  {
+    "revision": "889164edcdafdd808e1429a7b6bc431e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fedea50d3998d906609b",
+    "url": "/js/about.2737168a.js"
+  },
+  {
+    "revision": "7b721446d0a27cdea94d",
+    "url": "/js/app.945c0549.js"
+  },
+  {
+    "revision": "2d536c46d664fb2c15b2",
+    "url": "/js/chunk-vendors.05433243.js"
+  },
+  {
+    "revision": "8c3fc12ff8405cc29af9",
+    "url": "/js/login.8b9ee091.js"
+  },
+  {
+    "revision": "34c74c8c4b71f4992563d3456ace8696",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
